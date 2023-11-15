4b0383db5d361ff1cdcb3c678852f84450688eb2
evan <00.evanwilson@gmail.com>
2023-11-15 21:50:10 +0000

    finished mapa

---
+++ choroplethmap.py
@@ -0,0 +1,24 @@
+import plotly.graph_objects as go
+
+import pandas as pd
+
+df = pd.read_csv('https://github.com/marestaing/hosting/blob/main/visited_states.csv?raw=true')
+
+fig = go.Figure(data=go.Choropleth(
+    locations=df['code'], # Spatial coordinates
+    z = df['number students'].astype(float), # Data to be color-coded
+    locationmode = 'USA-states', # set of locations match entries in `locations`
+    colorscale = 'blackbody',
+    colorbar_title = "color scale title",
+))
+
+fig.update_layout(
+    title_text = 'Project 3',
+    geo_scope='usa', # limite map scope to USA
+)
+
+fig.show()
+
+
+
+
